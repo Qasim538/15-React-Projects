7dd5a1e3cd5b22ada89743a78ec326242b601b59
Qasim <mq12113@gmail.com>
2024-03-15 21:44:15 +0000

    menu item

--- 5-restaurant-menu/src/App.js
+++ 5-restaurant-menu/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import React, {useState} from 'react'
+import Categories from './Categories'
+import Menu from './Menu'
+import items from './data'
+
 
 function App() {
+  const {menuItems, setMenuItems} = useState(items);
+  const [categories, setCategories] =  useState([]) 
+
   return (
-    <div className="App">
-Hello World!
+  <main>
+    <section className='menu section'>
+    <div className="title">
+      <h2>Our Menu</h2>
+      <div className="underline"></div>
     </div>
+    <Categories />
+    <Menu items={menuItems}/>
+    </section>
+  </main>    
   );
 }
 
---
+++ 5-restaurant-menu/src/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <div>
+    <h2>      Categories Component
+    </h2>
+    </div>
+  )
+}
+
+export default Categories

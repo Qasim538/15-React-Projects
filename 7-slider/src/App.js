d635cd3260f2d369ac6950bf2e1a767719930f59
Qasim <mq12113@gmail.com>
2024-03-19 21:59:29 +0000

    set up new project of slider

---
+++ 7-slider/src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
+import { FaQuoreRight } from 'react-icons/fa';
+import data from './data'
+
+
+function App() {
+
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0)
+   return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span> Reviews
+        </h2>
+      </div>
+
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const {id, image, name, title, quote} = person;
+// more stuff coming up
+
+return (
+
+  <article key={id}>
+  <img src={image} alt={name} className="person-img"/>
+  
+  </article>
+)
+
+
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default App;

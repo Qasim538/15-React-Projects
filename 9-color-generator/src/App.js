2866870e962de0cee99a738c01edefaad8d9cbc9
Qasim <mq12113@gmail.com>
2024-03-24 22:13:16 +0000

    added basic functionality

--- 9-color-generator/src/App.js
+++ 9-color-generator/src/App.js
@@ -1,14 +1,37 @@
 import React, {useState} from 'react'
 import singlecolor from './SingleColor'
-import Values from 'values.js'
+
+// import Values from 'values.js'
 
 
 
 function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+   const [list, setList] = useState([]);
+
+   const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hello');
+   }
+
+
   return (
-    <div className="App">
-    <h3>Hello</h3>
-    </div>
+    <>
+    <section className="container">
+    <h3>color Generator</h3>
+    <form onSubmit={handleSubmit}>
+      <input type="text"  value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025'/>
+      <button className="btn" type='submit'>submit</button>
+    </form>
+    
+    </section>
+
+    <section className="colors">
+    
+    </section>
+    </>
+
   );
 }
 

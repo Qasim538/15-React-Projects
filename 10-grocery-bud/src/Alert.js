774a6874e53f2fed476c6ba7ff50b1f6e241ea05
Qasim <mq12113@gmail.com>
2024-04-14 20:51:51 +0100

    added alert section

--- 10-grocery-bud/src/Alert.js
+++ 10-grocery-bud/src/Alert.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
 
-const Alert = ({ type, msg, removeAlert, list }) => {
+import React, { useEffect } from 'react'
+
+const Alert = ({type, msg, removeAlert}) => {
   useEffect(() => {
     const timeout = setTimeout(() => {
-      removeAlert();
-    }, 3000);
-    return () => clearTimeout(timeout);
-  }, [list]);
-  return <p className={`alert alert-${type}`}>{msg}</p>;
-};
+      removeAlert()
+    })
+  },3000)
+  return (
+    <p className={`alert alert-${type}`}>{msg}</p>
+  )
+}
 
-export default Alert;
+export default Alert
